4dd859eab510b196b27a7a733fb91897be5094a6
Zoriana-Burian <ke7sha1991@gmail.com>
2020-11-07 22:09:14 +0200

    import+внесення правок

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import fetchApiCountries from './fetchCountries'
 import countryCard from '../templates/countries.hbs'
 import countriesListCard from '../templates/list-countries.hbs'
 import getRefs from './refs'
+import '@pnotify/core/dist/BrightTheme.css';
 
+const {error} = require('@pnotify/core');
 const debounce = require('lodash.debounce');
 
 const refs = getRefs();
@@ -11,17 +13,30 @@ refs.input.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
     e.preventDefault();
-    clearContainer()
-  
+   
+    clearContainer();
     const searchQuery = refs.input.value;
   
     fetchApiCountries(searchQuery)
       .then(renderCountry)
-      .catch(onFetchError)
+      .catch(onFetchError);
       
   }
 
-  function renderCountry() {
+  
+
+  function renderCountry(country) {
+      if(country.lenght > 10){
+          error({ text: "Too many matches found. Please enter a more specific query" }) 
+      } 
+      else if(country.lenght <= 10){
+        clearContainer();
+        createMarkup(countriesListCard, country)  
+      } 
+      else if(country.lenght === 1){
+        clearContainer();
+        createMarkup(countryCard, country)
+      }  
     
     
   }
@@ -30,7 +45,9 @@ function onSearch(e) {
     alert(error);
   }
 
-
+function createMarkup(temp ,country) {
+    refs.container.insertAdjacentHTML('afterbegin', temp(country))
+}
  function clearContainer() {
      refs.container.innerHTML = '';
   }
