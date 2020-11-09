eefcb511bb7f7d04671e05a9c0ff47e29f1b0dbc
Zoriana-Burian <ke7sha1991@gmail.com>
2020-11-09 19:51:02 +0200

    внесення правок до функції

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import fetchApiCountries from './fetchCountries'
-import countryCard from '../templates/countries.hbs'
-import countriesListCard from '../templates/list-countries.hbs'
-import getRefs from './refs'
+import fetchApiCountries from './fetchCountries';
+import countryCard from '../templates/countries.hbs';
+import countriesListCard from '../templates/list-countries.hbs';
+import getRefs from './refs';
 import '@pnotify/core/dist/BrightTheme.css';
 
 const {error} = require('@pnotify/core');
@@ -18,38 +18,39 @@ function onSearch(e) {
     const searchQuery = refs.input.value;
   
     fetchApiCountries(searchQuery)
-      .then(renderCountry)
-      .catch(onFetchError);
-      
+        .then(renderCountry)
+        .catch(error => console.log(error));
   }
 
-  
-
-  function renderCountry(country) {
-      if(country.lenght > 10){
-          error({ text: "Too many matches found. Please enter a more specific query" }) 
-      } 
-      else if(country.lenght <= 10){
-        clearContainer();
-        createMarkup(countriesListCard, country)  
-      } 
-      else if(country.lenght === 1){
-        clearContainer();
-        createMarkup(countryCard, country)
-      }  
-    
-    
-  }
-
-  function onFetchError(error) {
-    alert(error);
-  }
-
-function createMarkup(temp ,country) {
-    refs.container.insertAdjacentHTML('afterbegin', temp(country))
+ function renderCountry(country) {
+  if (country.length > 10) {
+    error({
+        text: "Too many matches found. Please enter a more specific query!"
+    });
+} else if (country.length > 1 && country.length <= 10) {
+  createMarkup(country, countriesListCard);
+} else if (country.length === 1) {
+  createMarkup(country, countryCard);
 }
- function clearContainer() {
-     refs.container.innerHTML = '';
-  }
+}
+ 
+function createMarkup(country, temp) {
+refs.container.insertAdjacentHTML('afterbegin',temp(country));
+}
+
+function clearContainer() {
+refs.container.innerHTML = '';
+}
   
+
+
+
+
+
+
+
+
+
+
+ 
 
